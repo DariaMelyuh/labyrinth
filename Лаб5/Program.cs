91ec67c43bb67b9fd2941ba69fcfda8caa71b881
Daria <rapssodia5@gmail.com>
2023-10-30 18:22:50 +0200

    message

--- Лаб5/Program.cs
+++ Лаб5/Program.cs
@@ -6,16 +6,10 @@ namespace MazeSingleton
     {
         static void Main(string[] args)
         {
-            //var factory1 = MazeGame.Create(SingletonMazeFactory.Instance);
-            //var factory2 = MazeGame.Create(SingletonMazeWithBombFactory.Instance);
-            //var factory3 = MazeGame.Create(SingletonMazeWithTrapFactory.Instance);
-
-           
             SingletonMazeFactory mazeFactory = SingletonMazeFactory.Instance;
             SingletonMazeWithBombFactory bombFactory = SingletonMazeWithBombFactory.Instance;
             SingletonMazeWithTrapFactory trapFactory = SingletonMazeWithTrapFactory.Instance;
 
-           
             Maze standardMaze = mazeFactory.CreateMaze();
             Maze mazeWithBomb = bombFactory.CreateMaze();
             Maze mazeWithTrap = trapFactory.CreateMaze();
