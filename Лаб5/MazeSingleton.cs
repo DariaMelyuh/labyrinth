d11a964f57100bc9c55e627e8053a651b3905393
Daria <rapssodia5@gmail.com>
2023-10-30 18:16:31 +0200

    message

---
+++ Лаб5/MazeSingleton.cs
@@ -0,0 +1,19 @@
+﻿using Лаб1;
+namespace MazeSingleton
+{
+    public class SingletonMazeFactory : MazeFactory
+    {
+        private static SingletonMazeFactory? _instance;
+
+        private SingletonMazeFactory() { }
+
+        public static SingletonMazeFactory Instance
+        {
+            get
+            {
+                _instance ??= new SingletonMazeFactory();
+                return _instance;
+            }
+        }
+    }
+}
