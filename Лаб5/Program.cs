bf794e16f22a8cb5ce19d7f3c0919ace3d96006f
Daria <rapssodia5@gmail.com>
2023-11-01 20:55:25 +0200

    message

--- Лаб5/Program.cs
+++ Лаб5/Program.cs
@@ -7,12 +7,7 @@ namespace MazeSingleton
         static void Main(string[] args)
         {
             SingletonMazeFactory mazeFactory = SingletonMazeFactory.Instance;
-            SingletonMazeWithBombFactory bombFactory = SingletonMazeWithBombFactory.Instance;
-            SingletonMazeWithTrapFactory trapFactory = SingletonMazeWithTrapFactory.Instance;
-
             Maze standardMaze = mazeFactory.CreateMaze();
-            Maze mazeWithBomb = bombFactory.CreateMaze();
-            Maze mazeWithTrap = trapFactory.CreateMaze();
         }
     }
 }
